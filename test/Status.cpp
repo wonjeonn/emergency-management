33c64343933203b47a8d5b076f23aed940ce11f4
wonjeonn <won200112@gmail.com>
2023-11-07 17:36:11 -0500

    add test for status

---
+++ test/Status.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include "Status.h"
+#include "Utils.h"
+
+namespace sdds {
+    Status::Status() {
+        m_desc = nullptr;
+        m_code = 0;
+    }
+
+    Status::Status(const char* desc) {
+        m_code = 0;
+        ut.alocpy(m_desc, desc);
+    }
+
+    Status::Status(const Status& Status) {
+        ut.alocpy(m_desc, Status.m_desc);
+        m_code = Status.m_code;
+    }
+
+    Status::~Status(){
+        clear();
+    }
+
+    Status& Status::operator=(const char* desc){
+        ut.alocpy(m_desc, desc);
+        return *this;
+    }
+
+        Status& Status::operator=(int code){
+        m_code = code;
+        return *this;
+    }
+
+    Status& Status::operator=(const Status& Status){
+        if (this != &Status) {
+            m_code = Status.m_code;
+            ut.alocpy(m_desc, Status.m_desc);
+        }
+        return *this;
+    }
+
+    Status::operator int() const{
+        return m_code;
+    }
+
+    Status::operator const char* () const {
+        return m_desc;
+    }
+
+    Status::operator bool() const{
+        return m_desc == nullptr;
+    }
+
+    Status& Status::clear() {
+        if (m_desc != nullptr) {
+            delete[] m_desc;
+            m_desc = nullptr;
+        }
+        m_code = 0;
+        return *this;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const Status& Status) {
+        if ((int)Status != 0) {
+            os << "ERR#" << (int)Status << ": ";
+        }
+        os << (const char*)Status;
+        return os;
+    }
+}
---
+++ test/Utils.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include "Utils.h"
+
+using namespace std;
+namespace sdds {
+    Utils ut;
+    void Utils::testMode(bool testmode) {
+        m_testMode = testmode;
+    }
+
+    void Utils::getSystemDate(int* year, int* mon, int* day) {
+        if (m_testMode) {
+            if(day) *day = sdds_testDay;
+            if(mon) *mon = sdds_testMon;
+            if(year) *year = sdds_testYear;
+        } else {
+            time_t t = std::time(NULL);
+            tm lt = *localtime(&t);
+            if (day) *day = lt.tm_mday;
+            if (mon) *mon = lt.tm_mon + 1;
+            if (year) *year = lt.tm_year + 1900;
+        }
+    }
+
+    int Utils::daysOfMon(int month, int year)const {
+        int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
+        int mon = (month >= 1 && month <= 12 ? month : 13)-1;
+        return days[mon] + int((mon == 1) * ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+    }
+
+    void Utils::alocpy(char*& destination, const char* source) {
+        delete[] destination;
+        destination = nullptr;
+        if (source) {
+            destination = new char[strlen(source) + 1];
+            strcpy(destination, source);
+        }
+    }
+
+    int Utils::getint(const char* prompt) {
+        int value = 0;
+        bool isInt = false;
+        
+        if (prompt && prompt[0]) {
+            cout << prompt;
+        }
+        
+        while (!isInt) {
+            cin >> value;
+            
+            if (cin.fail()) {
+                cin.clear();
+                cout << "Invalid Integer, retry: ";
+                cin.ignore(10000, '\n');
+            } else {
+                if (cin.get() != '\n') {
+                    cout << "Enter an integer only please, retry: ";
+                    cin.ignore(10000, '\n');
+                } else {
+                    isInt = true;
+                }
+            }
+        }
+        return value;
+    }
+
+    int Utils::getint(int min, int max, const char* prompt, const char* errMes) {
+        int value = 0;
+        bool isRange = false;
+        
+        while (!isRange) {
+            value = getint(prompt);
+            if (value < min || value > max) {
+                if (errMes) {
+                    cout << errMes << ", retry: ";
+                } else {
+                    cout << "Value out of range [" << min << "<=val<=" << max << "]: ";
+                    prompt = nullptr;
+                }
+            } else {
+                isRange = true;
+            }
+        }
+        return value;
+    }
+}
